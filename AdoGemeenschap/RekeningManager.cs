3fed4795281564681b14d6daee7170de1c990ea4
davyraes <davy-raes@telenet.be>
2018-01-04 15:56:41 +0100

    einde dag

--- AdoGemeenschap/RekeningManager.cs
+++ AdoGemeenschap/RekeningManager.cs
@@ -33,17 +33,74 @@ namespace AdoGemeenschap
                 {
                     comStorten.CommandType = CommandType.StoredProcedure;
                     comStorten.CommandText = "Storten";
+
                     DbParameter parTeStorten = comStorten.CreateParameter();
                     parTeStorten.ParameterName = "@teStorten";
                     parTeStorten.Value = teStorten;
                     parTeStorten.DbType = DbType.Currency;
                     comStorten.Parameters.Add(parTeStorten);
+
                     DbParameter parRekeningNr = comStorten.CreateParameter();
                     parRekeningNr.ParameterName = "@RekeningNr";
                     parRekeningNr.Value = rekeningNr;
                     comStorten.Parameters.Add(parRekeningNr);
                     conBank.Open();
                     return comStorten.ExecuteNonQuery() != 0;
+                }
+            }
+        }
+        public void Overschrijven(decimal bedrag,string vanRekening,string naarRekening)
+        {
+            var dbManager = new BankDbManager();
+            using (var conBank = dbManager.GetConnection())
+            {
+                conBank.Open();
+                using (var traOverschrijven = conBank.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    using (var comAftrekken = conBank.CreateCommand())
+                    {
+                        comAftrekken.Transaction = traOverschrijven;
+                        comAftrekken.CommandType = CommandType.Text;
+                        comAftrekken.CommandText = "update Rekeningen set Saldo=Saldo-@bedrag where RekeningNr=@reknr";
+
+                        DbParameter parBedrag = comAftrekken.CreateParameter();
+                        parBedrag.ParameterName = "@bedrag";
+                        parBedrag.Value = bedrag;
+                        comAftrekken.Parameters.Add(parBedrag);
+
+                        DbParameter parRekening = comAftrekken.CreateParameter();
+                        parRekening.ParameterName = "@reknr";
+                        parRekening.Value = vanRekening;
+
+                        if (comAftrekken.ExecuteNonQuery()==0)
+                        {
+                            traOverschrijven.Rollback();
+                            throw new Exception("Van rekening bestaat niet");
+                        }
+                    }
+                    using (var comBijtellen = conBank.CreateCommand())
+                    {
+                        comBijtellen.Transaction = traOverschrijven;
+                        comBijtellen.CommandType = CommandType.StoredProcedure;
+                        comBijtellen.CommandText = "Storten";
+
+                        DbParameter parbedrag = comBijtellen.CreateParameter();
+                        parbedrag.ParameterName = "@teStorten";
+                        parbedrag.Value = bedrag;
+                        comBijtellen.Parameters.Add(parbedrag);
+
+                        DbParameter parRekening = comBijtellen.CreateParameter();
+                        parRekening.ParameterName = "@rekeningNr";
+                        parRekening.Value = naarRekening;
+                        comBijtellen.Parameters.Add(parRekening);
+                         
+                        if (comBijtellen.ExecuteNonQuery()==0)
+                        {
+                            traOverschrijven.Rollback();
+                            throw new Exception("Naar Rekening bestaat niet");
+                        }
+                    }
+                    traOverschrijven.Commit();
                 }
             }
         }
---
+++ AdoWPF/Overschrijven.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AdoGemeenschap;
+
+namespace AdoWPF
+{
+    /// <summary>
+    /// Interaction logic for Overschrijven.xaml
+    /// </summary>
+    public partial class Overschrijven : Window
+    {
+        public Overschrijven()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOverschrijven_Click(object sender, RoutedEventArgs e)
+        {
+            decimal bedrag;
+            if ( decimal.TryParse(textboxBedrag.Text,out bedrag))
+            {
+                try
+                {
+                    var dbManager = new BankDbManager();
+
+                }catch(Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
