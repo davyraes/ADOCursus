b72b6e84fb66a7ff8cdcce973fecd7f15933eb1b
davyraes <davy-raes@telenet.be>
2018-01-09 09:21:15 +0100

    wissel

---
+++ AdoGemeenschap/KlantManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AdoGemeenschap
+{
+    public  class KlantManager
+    {
+        public Int64 NieuweKlant(string naam)
+        {
+            var manager = new BankDbManager();
+            using (var conBank = manager.GetConnection())
+            {
+                using (var comToevoegen = conBank.CreateCommand())
+                {
+                    comToevoegen.CommandType = CommandType.StoredProcedure;
+                    comToevoegen.CommandText = "NieuweKlant";
+
+                    var parNaam = comToevoegen.CreateParameter();
+                    parNaam.ParameterName = "@naam";
+                    parNaam.Value = naam;
+                    comToevoegen.Parameters.Add(parNaam);
+
+                    conBank.Open();
+                    return Convert.ToInt64(comToevoegen.ExecuteScalar());
+                }
+            }
+        }
+    }
+}
